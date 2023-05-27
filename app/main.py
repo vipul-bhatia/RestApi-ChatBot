46e7f4d1810eafb0b4fec901745d98b9de86e37f
vipul-bhatia <vipul0592bhatia@gmail.com>
2023-05-27 15:00:39 +0530

    .

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI 
 from pydantic import BaseModel
-from model.model import predict_pipeline 
-from model.model import __version__ as model_version
+from app.model.model import predict_pipeline 
+from app.model.model import __version__ as model_version
 
 app = FastAPI()
 
